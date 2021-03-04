0a54b123f5a37d3ef21a76e342be8bd949650ddc
marciojrnunes <marcio-junior@hotmail.com>
2021-03-04 07:47:49 -0300

    Updated to talk about class members

--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -4,15 +4,17 @@ class Pessoa {
     var nome: String = "Jether"
     var cpf: String = "123.123.123-12"
         private set
+
+    fun pessoaInfo() = "$nome e cpf: $cpf"
+
 }
 
 fun main(){
     val jether = Pessoa()  //Variavel (objeto) jether recebe a instancia do classe Pessoa
 
-    jether.nome = "4567"
+    //println(jether.nome)
+    //println(jether.cpf)
 
-    println(jether.nome)
-    println(jether.cpf)
-
+    println(Pessoa().pessoaInfo())
 
 }
