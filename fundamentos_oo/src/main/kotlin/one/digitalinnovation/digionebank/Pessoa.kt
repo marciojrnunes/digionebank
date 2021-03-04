786b0a66e773dc0efc3d0a23ac1bc1da2e2f03b4
marciojrnunes <marcio-junior@hotmail.com>
2021-03-04 08:22:46 -0300

    Added println to nome and cpf

--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -12,8 +12,8 @@ class Pessoa {
 fun main(){
     val jether = Pessoa()  //Variavel (objeto) jether recebe a instancia do classe Pessoa
 
-    //println(jether.nome)
-    //println(jether.cpf)
+    println(jether.nome)
+    println(jether.cpf)
 
     println(Pessoa().pessoaInfo())
 
