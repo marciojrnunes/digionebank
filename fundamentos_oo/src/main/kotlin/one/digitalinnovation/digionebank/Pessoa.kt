073167ec550703ca15d5328750fb9f92b9acd958
marciojrnunes <marcio-junior@hotmail.com>
2021-03-06 09:07:24 -0300

    updated to make person and employee abstract class

--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,23 +1,7 @@
 package one.digitalinnovation.digionebank
 
-class Pessoa {
-    var nome: String = "Jether"
-    var cpf: String = "123.123.123-12"
-        private set
+abstract class Pessoa (
+        val nome: String,
+        val cpf: String
+        )
 
-    constructor()
-
-    fun pessoaInfo() = "$nome e cpf: $cpf"
-
-}
-
-fun main(){
-    val jether = Pessoa()  //Variavel (objeto) jether recebe a instancia do classe Pessoa
-
-    //println(jether.nome)
-    //println(jether.cpf)
-
-    println(jether.pessoaInfo())
-
-
-}
