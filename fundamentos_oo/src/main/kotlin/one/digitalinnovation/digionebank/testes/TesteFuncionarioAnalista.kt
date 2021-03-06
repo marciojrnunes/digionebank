f93322deb0217930bc34de0b8eccbe5619d48c42
marciojrnunes <marcio-junior@hotmail.com>
2021-03-06 09:56:46 -0300

    created tests and report printer

---
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioAnalista.kt
@@ -0,0 +1,14 @@
+package one.digitalinnovation.digionebank.testes
+
+import one.digitalinnovation.digionebank.Analista
+
+fun main() {
+
+    val joao = Analista(nome = "Joao Rodrigues", cpf = "987654321-99", 2000.0)
+    //println(joao.nome)
+    //println(joao.cpf)
+    //println(joao.salario)
+
+    ImprimeRelatorioFuncionario.imprime(joao)
+
+}
---
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
@@ -0,0 +1,14 @@
+package one.digitalinnovation.digionebank.testes
+
+import one.digitalinnovation.digionebank.Gerente
+
+fun main() {
+
+    val maria = Gerente(nome = "Maria do Carmo", cpf = "123789465-74", 5400.0)
+    //println(joao.nome)
+    //println(joao.cpf)
+    //println(joao.salario)
+
+    ImprimeRelatorioFuncionario.imprime(maria)
+
+}
